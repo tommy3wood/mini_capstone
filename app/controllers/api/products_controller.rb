c5b680744131ce474919b775224dd61c68df44b6
tommy3wood <thomaswcurran1014@gmail.com>
2020-01-15 11:25:33 -0600

    individual products/based on price

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,4 +5,36 @@ class Api::ProductsController < ApplicationController
     render "all_products.json.jb"
   end
 
+  def budget_products
+    budget_picks = []
+    index = 0
+
+    while index < Product.all.length
+      if Product.all[index].price < 110
+        budget_picks << Product.all[index]
+      end
+      index += 1
+    end
+
+    @budget_products = budget_picks
+
+    render "budget_products.json.jb"
+  end
+
+  def high_end_products
+    high_end_picks = []
+    index = 0
+
+    while index < Product.all.length
+      if Product.all[index].price > 110
+        high_end_picks << Product.all[index]
+      end
+      index += 1
+    end
+
+    @high_end_products = high_end_picks
+
+    render "high_end_products.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/all_products' => 'products#all_products'
+    get '/budget' => 'products#budget_products'
+    get '/high_end' => 'products#high_end_products'
+    get '/lifestyle' => 'products#lifestyle_products'
+    get '/basketball' => 'products#basketball_products'
   end
 end
