7689e7b62e184e4050dcf114ea163f9ec99b2e45
tommy3wood <thomaswcurran1014@gmail.com>
2020-01-22 11:16:04 -0600

    validation exercises

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -12,8 +12,11 @@ class Api::ProductsController < ApplicationController
                 image_url: params[:image_url],
                 description: params[:description]
                 )
-    @product.save
-    render 'show.json.jb'
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -29,8 +32,11 @@ class Api::ProductsController < ApplicationController
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
 
-    @product.save
-    render 'show.json.jb'
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,23 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: {maximum: 255}
+  validates :description, length: {in: 10..500}
+  # validates :description, length: { minimum: 10, maximum: 500 } fair alt to (in: 10..500)
+  validates :image_url, length: {maximum: 255}
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0, less_than: 99_999.99 }
 
-def is_discounted?
-  price < 100
-end
+  def is_discounted?
+    price < 100
+  end
 
-def tax
-  price * 0.09
-end
+  def tax
+    price * 0.09
+  end
 
-def total_price
-  tax + price
-end
+  def total_price
+    tax + price
+  end
 
 end
