9602dc1315b3203b8e25077d89f39b24eb642e81
tommy3wood <thomaswcurran1014@gmail.com>
2020-01-23 12:33:22 -0600

    active record query adds

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,28 @@
 class Api::ProductsController < ApplicationController
 
   def index
+    search_term = params[:search]
+    discount_search = params[:discount]
+    sort_action = params[:sort]
+    sort_order = params[:sort_order]
     @products = Product.all
+
+    if search_term
+      @products = @products.where("name iLIKE ?", "%#{search_term}%")
+    end
+
+    if discount_search == "true"
+      @products = @products.where("price < ?", 100)
+    end
+
+    if sort_order && sort_action
+      @products = @products.order(sort_action => sort_order)
+    elsif sort_action
+      @products = @products.order(sort_action)
+    else
+      @products = @products.order(id: :asc)
+    end
+
     render 'index.json.jb'
   end
 
